15e4f8ecc5a69f3a6307473b308796d4e07e1dde
hoodielive <hoodielive@gmail.com>
2018-04-01 14:02:33 -0400

    compile with rustc or cargo build

---
+++ rustSample/target/debug/deps/hello_rust-c2436dba473fc518.d
@@ -0,0 +1,5 @@
+/home/lucid/Clones/rust/rustSample/target/debug/deps/hello_rust-c2436dba473fc518: src/main.rs
+
+/home/lucid/Clones/rust/rustSample/target/debug/deps/hello_rust-c2436dba473fc518.d: src/main.rs
+
+src/main.rs:
