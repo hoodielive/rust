143a830b42e945fa8359fb176e0ded8f6f954c2b
hoodielive <hoodielive@gmail.com>
2018-04-03 23:05:13 -0400

    dummy file

---
+++ simple_function/target/debug/deps/simple_function-74b89ad33ac15601.d
@@ -0,0 +1,5 @@
+/Users/blackmagus/Git_Clones/rust/simple_function/target/debug/deps/libsimple_function-74b89ad33ac15601.rlib: src/lib.rs
+
+/Users/blackmagus/Git_Clones/rust/simple_function/target/debug/deps/simple_function-74b89ad33ac15601.d: src/lib.rs
+
+src/lib.rs:
---
+++ simple_function/target/debug/deps/simple_function-f99e8842819ca23c.d
@@ -0,0 +1,5 @@
+/Users/blackmagus/Git_Clones/rust/simple_function/target/debug/deps/simple_function-f99e8842819ca23c: src/main.rs
+
+/Users/blackmagus/Git_Clones/rust/simple_function/target/debug/deps/simple_function-f99e8842819ca23c.d: src/main.rs
+
+src/main.rs:
