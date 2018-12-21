bc5c590c737bb73a45476b593af4d813165182d4
hoodielive <hoodielive@gmail.com>
2018-12-21 12:50:06 -0500

    Rust Notes -- <s

---
+++ study.rs
@@ -0,0 +1,41 @@
+// a variable is called a binding in rust and it is immutable!
+
+let var = "value";
+
+let mut counter = 0;
+counter = counter + 1;
+counter = counter + 10;
+
+// You will not be able to use variables that are uninitialized
+/**
+
+- Rust has a strong, static type system
+- Strong means it doesn't automatically turn one type into another
+- Static means it knows what type things are before running the program
+
+Static vs Dynamic
+Weak vs Strong
+
+Static-Weak:
+- C
+- C++
+
+Dynamic-Weak
+- Perl
+- PHP
+- VB
+- JavaScript
+
+Static-Strong
+- Haskell
+- C#
+- F#
+- Rust
+
+Dynamic-Strong
+- Ruby
+- Clojure
+- Python
+- Erlang
+
+ */
