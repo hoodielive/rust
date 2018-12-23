3c762a63a91fcd46e619572c7d11dc20e0acea5f
hoodielive <hoodielive@gmail.com>
2018-12-23 07:24:33 -0500

    dynamic fibonacci corrected -> u64 error fixed

--- dynamic-fibonacci/src/main.rs
+++ dynamic-fibonacci/src/main.rs
@@ -13,14 +13,14 @@ fn fib(n: u64) -> u64 {
     }
 }
 
-fn fib_dyn(n: u64, map: &mut HashMap<u64, u64>) {
+fn fib_dyn(n: u64, map: &mut HashMap<u64, u64>) -> u64 {
     match n {
         0 | 1 => 1, 
-        n =>  {
+        n => {
             if map.contains_key(&n) {
                 *map.get(&n).unwrap() 
             } else { 
-                let val = fib_dyn(n-1, map) + fib_dyn(n-2, map) 
+                let val = fib_dyn(n-1, map) + fib_dyn(n-2, map);
                 map.insert(n, val); 
                 val
             }
@@ -29,7 +29,7 @@ fn fib_dyn(n: u64, map: &mut HashMap<u64, u64>) {
 }
 
 fn main() {
-    let mut map = HashMap::new(); 
+    let mut map = HashMap::new();
     for i in 1..41 {
         println!("{}: {}", i, fib_dyn(i, &mut map))
     }
---
+++ dynamic-fibonacci/target/debug/dynamic-fibonacci.d
@@ -0,0 +1,1 @@
+/home/ogun/Clones/rust/dynamic-fibonacci/target/debug/dynamic-fibonacci: /home/ogun/Clones/rust/dynamic-fibonacci/src/main.rs
