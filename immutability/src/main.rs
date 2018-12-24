d17188ecf88190abb0ce3e3a1defc0ac1e3d8c56
hoodielive <hoodielive@gmail.com>
2018-12-24 10:27:53 -0500

    tup: (i8, u8, f32)

--- immutability/src/main.rs
+++ immutability/src/main.rs
@@ -1,5 +1,11 @@
 fn main() {
     let x: u32 = 5; 
+    
     // x is an immutable variable with the value 5
-    println!("The value of x is: {}", x)
+    println!("The value of x is: {}", x);
+
+    let x2 = "a";
+    println!("The char is: {}", x2);
+
+    let tup: (u8, i8, f32) = (1, -1, 3.5); 
 }
