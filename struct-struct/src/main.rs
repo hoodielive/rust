1156730466a4cf453ca3e597bb62f537c4605c76
hoodielive <hoodielive@gmail.com>
2018-12-24 11:27:06 -0500

    points at where

--- struct-struct/src/main.rs
+++ struct-struct/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+struct Point 
+{
+    x: f64, 
+    y: f64
+}
+
+fn structures()
+{
+    let p = Point { x: 3.0, y: 4.0 }; 
+    println!("point p is at ({}, {})", p.x, p.y); 
+}
+
+fn main() 
+{
+    structures();
 }
---
+++ struct-struct/target/debug/struct-struct.d
@@ -0,0 +1,1 @@
+/home/ogun/Clones/rust/struct-struct/target/debug/struct-struct: /home/ogun/Clones/rust/struct-struct/src/main.rs
