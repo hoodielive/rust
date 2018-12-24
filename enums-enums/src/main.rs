df39ac2c2d6a420934c7c30ae3051c618eff3be0
hoodielive <hoodielive@gmail.com>
2018-12-24 11:39:12 -0500

    enum Color::RgbColor(0,0,0)

--- enums-enums/src/main.rs
+++ enums-enums/src/main.rs
@@ -1,3 +1,25 @@
+enum Color 
+{
+    Red, 
+    Green,
+    Blue, 
+    RgbColor(u8,u8,u8)
+}
+
+fn enums()
+{
+//    let c:Color = Color::Red; 
+    let d:Color = Color::RgbColor(10,0,0);
+    match d
+    {
+        Color::Red => println!("r"),
+        Color::Green => println!("g"),
+        Color::Blue => println!("b"),
+        Color::RgbColor(0,0,0) => println!("black"),
+        Color::RgbColor(r,g,b) => println!("rgb({},{},{})", r,g,b),
+        _ => println!("some other color")
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    enums(); 
 }
---
+++ enums-enums/target/debug/deps/enums_enums-1d4dee959d9207f7.d
@@ -0,0 +1,5 @@
+/home/ogun/Clones/rust/enums-enums/target/debug/deps/enums_enums-1d4dee959d9207f7: src/main.rs
+
+/home/ogun/Clones/rust/enums-enums/target/debug/deps/enums_enums-1d4dee959d9207f7.d: src/main.rs
+
+src/main.rs:
