2fbdba2ea525ae2ce4a79c92e4f272f7cbfeb396
hoodielive <hoodielive@gmail.com>
2018-12-24 11:56:07 -0500

    enums are great :)

--- enums-enums/src/main.rs
+++ enums-enums/src/main.rs
@@ -3,21 +3,25 @@ enum Color
     Red, 
     Green,
     Blue, 
-    RgbColor(u8,u8,u8)
+    RgbColor(u8,u8,u8), // tuple 
+    CmykColor{cyan:u8, magenta:u8, yellow:u8, black:u8}, // struct
 }
 
 fn enums()
 {
-//    let c:Color = Color::Red; 
+    let c:Color = Color::Red; 
     let d:Color = Color::RgbColor(10,0,0);
-    match d
+    let e:Color = Color::CmykColor{cyan: 0, magenta: 128, yellow: 0, black: 0};
+
+    match e
     {
         Color::Red => println!("r"),
         Color::Green => println!("g"),
         Color::Blue => println!("b"),
         Color::RgbColor(0,0,0) => println!("black"),
-        Color::RgbColor(r,g,b) => println!("rgb({},{},{})", r,g,b),
-        _ => println!("some other color")
+        | Color::CmykColor{cyan:_, magenta:_, yellow:_, black:255} => println!("black"),
+        Color::RgbColor(r,g,b) => println!("rgb({},{},{})", r,g,b), 
+        _ => ()
     }
 }
 fn main() {
