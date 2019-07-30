73e9765bb722820bb8cde067704956f688eed922
hoodielive <hoodielive@gmail.com>
2019-07-30 18:16:40 -0400

    what is this?

---
+++ guess/target/debug/deps/guess-fa7d25b5cfa828dd.d
@@ -0,0 +1,5 @@
+/Users/tzyurash/Clones/rust/guess/target/debug/deps/guess-fa7d25b5cfa828dd: src/main.rs
+
+/Users/tzyurash/Clones/rust/guess/target/debug/deps/guess-fa7d25b5cfa828dd.d: src/main.rs
+
+src/main.rs:
--- guess/target/debug/guess.d
+++ guess/target/debug/guess.d
@@ -1,1 +1,1 @@
-/home/ogun/Clones/rust/guess/target/debug/guess: /home/ogun/Clones/rust/guess/src/main.rs
+/Users/tzyurash/Clones/rust/guess/target/debug/guess.dSYM: /Users/tzyurash/Clones/rust/guess/src/main.rs
