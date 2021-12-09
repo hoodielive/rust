640ba1b35be1252fca67f8acb2714dea66778d01
hoodielive <hoodielive@gmail.com>
2021-12-09 01:28:35 -0500

    Add, lester.

--- c_and_rust/lester/src/main.rs
+++ c_and_rust/lester/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Write};
+use std::f64;
+
+fn main()
+{
+    println!("Print something yo.. ");
+    println!(); 
+    println!("Arguments can be referred to by their position: {0}, {1}! and {1}, {0}! are built from the same arguments", "dope", "fantastic");
+    println!("");
 }
---
+++ c_and_rust/lester/target/debug/lester.d
@@ -0,0 +1,1 @@
+/home/arjuna/Clones/rust/c_and_rust/lester/target/debug/lester: /home/arjuna/Clones/rust/c_and_rust/lester/src/main.rs
