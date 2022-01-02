08fd112aee3f1a6ff58350aadd75fbd3a3300873
hoodielive <hoodielive@gmail.com>
2022-01-02 15:37:09 -0500

    conditionals

--- 2022/lets_go/src/conditionals.rs
+++ 2022/lets_go/src/conditionals.rs
@@ -2,18 +2,19 @@ pub fn run()
 {
     let age: u8 = 18;
     let _check_id: bool = false;
+    let _knows_person_of_age: bool = true;
 
 
-    //If/Else
-    if age >= 21 && check_id
+    if age >= 21 && _check_id
     {
         println!("Bartender: What would you like to drink?");
     }
-    else if age < 21 && check_id {
+    else if age < 21 && _check_id || _knows_person_of_age {
         println!("You are not old enough to drink.");
     }
     else {
         
         println!("I'll need to see your ID.");
     }
+
 }
