922ed80d458a705151b3db0cb964d4da487407f3
hoodielive <hoodielive@gmail.com>
2022-04-19 21:15:39 -0400

    Playing..

---
+++ 2022/slime/src/justapanic.rs
@@ -0,0 +1,6 @@
+fn degrae()
+{
+    let crash_reason = "Server wanted a nap.";
+    panic!("I crashed {}", crash_reasonn);
+    println!("this will never run!");
+}
--- 2022/slime/src/main.rs
+++ 2022/slime/src/main.rs
@@ -1,7 +1,15 @@
+
+
 fn main() {
     let greeting = "Howdy";
     let subject = "Aeon";
-    let formatted_greeting = format!("{}, {}!", greeting, subject);
+    let formated_math =format!("{:04}", 42);
+    let formatted_principles = format!("{}, {}!", greeting, subject);
 
-    print!(formatted_greeting);
+    println!("Hello {formatted_principles}");
+    format!("hello");
+    format!("hello, {}!", 1);
+    format!("The number is {}", 1);
+    format!("{:?}", (3, 4));
+    println!("This is an underwhelming view of an {formated_math}")
 }
